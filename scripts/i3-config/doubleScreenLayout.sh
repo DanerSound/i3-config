3a747ef645451a8fe9ac3d0b930041969a336561
DanerSound <andre.barreto28@gmail.com>
2018-08-14 19:28:46 +0200

    update double screen config bash

---
+++ scripts/i3-config/doubleScreenLayout.sh
@@ -0,0 +1,2 @@
+#DoubleScreen Layout HOME
+xrandr --output VGA-1 --mode 1024x768 --pos 0x0 --rotate normal --output LVDS-1-2 --off --output HDMI-1 --off --output HDMI-1-2 --off --output LVDS-1 --primary --mode 1366x768 --pos 1024x0 --rotate normal --output VGA-1-2 --off --output DP-1 --off
