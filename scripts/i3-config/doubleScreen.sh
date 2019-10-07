72cadc6e9180b3ccaf0fa1e8a76181aae16d52b5
Andre Cristhian <andre.barreto28@gmail.com>
2019-10-07 23:53:54 +0200

    update of 2019

---
+++ scripts/i3-config/doubleScreen.sh
@@ -0,0 +1,11 @@
+#DoubleScreen Layout VGA HOME Asus
+exec xrandr --output LVDS-1-1 --primary --mode 1366x768 --pos 1440x0 --rotate normal --output HDMI-1-1 --off --output LVDS-0 --off --output VGA-1-1 --mode 1440x900 --pos 0x0 --rotate normal --output HDMI-0 --off --output DP-1-1 --off &
+
+exec restart
+
+
+
+
+
+
+
